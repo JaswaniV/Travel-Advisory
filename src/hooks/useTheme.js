630ce44e2d2c64434029ea39603b9e9b344686cf
JaswaniV <vjaswan78@gmail.com>
2026-08-29 00:53:29 +0530

    fix: correct useEffect cleanup in theme toggle

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -3,6 +3,8 @@ import { useLocalStorage } from './useLocalStorage';
 
 export function useTheme() {
   const [theme, setTheme] = useLocalStorage('voyager-theme', 'light');
-  useEffect(() => document.documentElement.dataset.theme = theme, [theme]);
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+  }, [theme]);
   return [theme, () => setTheme((current) => (current === 'light' ? 'dark' : 'light'))];
 }
